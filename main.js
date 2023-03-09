e7fa364962c1e2551db4a2a08210340ab624c7b4
makhmudoff <gachon031698@gmail.com>
2023-03-09 23:13:47 +0900

    I finished main.js section I used very easy way

---
+++ main.js
@@ -0,0 +1,26 @@
+"use strict";
+
+const modal = document.querySelector(".modal");
+
+const overlay = document.querySelector(".overlay");
+
+const showModal = document.querySelectorAll(".show-modal");
+
+const closeModal = document.querySelector(".close-modal");
+
+const openModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+const btnCloseModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+for (let i = 0; i < showModal.length; i++) {
+  showModal[i].addEventListener("click", openModal);
+}
+
+modal.addEventListener("click", btnCloseModal);
+overlay.addEventListener("click", btnCloseModal);
